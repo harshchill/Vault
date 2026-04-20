f4f485dc8197c29d9124a5e7dc0d8631ac2ab057
harshchill <harshmahto02@gmail.com>
2026-04-20 07:02:06 +0530

    feat: add Vercel Speed Insights integration

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SessionProvider from "./providers/SessionProvider";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Navbar from "./component/Navbar";
 
 const geistSans = Geist({
@@ -41,6 +42,7 @@ export default function RootLayout({ children }) {
             <main>
               {children}
               <Analytics />
+              <SpeedInsights />
             </main>
           </SessionProvider>
         </div>
